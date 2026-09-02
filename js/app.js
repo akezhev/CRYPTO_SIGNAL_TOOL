a3d8f58332f6afc40e27d681381593119535ef56
akezhev <akezh@mail.ru>
2026-09-02 08:40:18 +0300

    added signal-count on the start page

--- js/app.js
+++ js/app.js
@@ -1475,6 +1475,8 @@ class UIRenderer {
                       )
                       .join("")}
                 </div>
+                <div class="stat-item">⚡ <span id="signal-count">0</span> сигналов</div>
+              
 
                 <div class="signal-grid" id="signal-grid">
                     ${assetCards}
